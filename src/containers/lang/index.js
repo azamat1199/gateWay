2c03bc22a1e0d09c25fcb25d9f8c1aecfeb8032c
= <=>
2021-11-17 19:36:54 +0500

    Lang

---
+++ src/containers/lang/index.js
@@ -0,0 +1,25 @@
+// import ru from "./ru.json";
+// import uz from "./uz.json";
+// import en from "./en.json";
+// import en from "./en.json"
+// import store from "../../../src/store";
+
+// const dictionary = {
+//   uz,
+//   ru,
+//   en,
+// };
+
+// function getCurrentlang() {
+//   return store.getState().user.user.lang || "ru";
+// }
+
+// function t(str) {
+//   const lang = store.getState().user.lang || "ru";
+//   console.log(lang);
+//   const translation = dictionary[lang][str];
+//   console.log(translation);
+//   return translation || +str;
+// }
+
+// export { t as default, getCurrentlang };
