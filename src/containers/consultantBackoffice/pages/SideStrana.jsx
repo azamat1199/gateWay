b11be5b44a8f77142fe62dfcffb0659c774ec5c4
= <=>
2021-11-24 23:37:06 +0500

    SideStrana

--- src/containers/consultantBackoffice/pages/SideStrana.jsx
+++ src/containers/consultantBackoffice/pages/SideStrana.jsx
@@ -1,141 +1,99 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Loader from "react-js-loader";
 
-// import img 
-import russiaFlag from  "../../../assets/icon/russiaFlag.png"  
-import usaFlag from     "../../../assets/icon/usaFlag.png"  
+// import img
+import russiaFlag from "../../../assets/icon/russiaFlag.png";
+import usaFlag from "../../../assets/icon/usaFlag.png";
+import blueStroke from "../../../assets/images/Stroke-blue.svg";
 
 // import css
-import "../../../style/css/SideStrana.css"
-import Sidebar from './SidebarConsult';
+import "../../../style/css/SideStrana.css";
+import Sidebar from "./SidebarConsult";
+import Axios from "../../../utils/axios";
+import axios from "axios";
 
-const SideStrana = () => {
-    return ( 
-        <Sidebar>
-        <div className="asos">
-            <div className="Up_navbar">
-                <h4>Cтраны</h4>
-                <div>
-                    <img src="https://picsum.photos/70" alt="" />
-                    <div>
-                        <h5>Nargiza Akhmedova</h5>
-                        <p>IT Specialist</p>
-                    </div>
-                </div>
+function SideStrana() {
+  const [countryList, setCountryList] = useState([]);
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    let mounted = true;
+    Axios.get("common/country/list/").then((res) => {
+      if (mounted) {
+        setCountryList(res.data.results);
+        setLoading(false);
+      }
+    });
+    return () => {
+      mounted = false;
+      setLoading(false);
+    };
+  }, []);
+
+  const selector = useSelector((state) => state.payload.payload.data);
+
+  return (
+    <div className="ConsultSideStrana">
+      <Sidebar>
+        <div className="asos" id="top">
+          <div className="Up_navbar">
+            <h4>Cтраны</h4>
+            <div>
+              <img src="https://picsum.photos/70" alt="" />
+              <div>
+                <h5>
+                  {" "}
+                  {selector.first_name} {selector.last_name}
+                </h5>
+                <h5>
+                  {(selector.role == "branch_director" && "директор филиала") ||
+                    selector.role}
+                </h5>
+              </div>
             </div>
-        <div className="SideStrana">
+          </div>
+          <div className="SideStrana">
             <div className="addNote">
-                <input type="text" placeholder="Добавьте заметку сотруднику"/>
+              <input type="text" placeholder="Добавьте заметку сотруднику" />
             </div>
-            <button>Добавить страну</button>
+            {(selector.role == "branch_director" && <span></span>) || (
+              <button>Добавить страну</button>
+            )}
             <div className="blockCountry">
-                <h4>Список стран</h4>
-                <div className="listCountry">
-                    {/*  */}
+              <h4>Список стран</h4>
+              <div className="listCountry">
+                {loading ? (
+                  <Loader
+                    type="spinner-circle"
+                    bgColor={"#FFFFFF"}
+                    color={"#FFFFFF"}
+                    size={80}
+                  />
+                ) : (
+                  countryList.map((x) => (
                     <div>
-                        <img src={russiaFlag} alt="" />
-                        <p>Испания</p>
+                      {/*image yoqligi sabab , xozircha image static*/}
+                      <div className="image-strana-div">
+                        <img src={x.image} alt="" />
+                      </div>
+                      <div>
+                        <p>{x.name}</p>
+                      </div>
                     </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                    {/*  */}
-                    <div>
-                        <img src={usaFlag} alt="" />
-                        <p>Испания</p>
-                    </div>
-                </div>
+                  ))
+                )}
+              </div>
             </div>
+          </div>
+          <a href="#top" title="Go to top" className="backTop">
+            <img src={blueStroke} alt="back to top" />
+          </a>
         </div>
-        </div>
-        </Sidebar>
-    );
+      </Sidebar>
+    </div>
+  );
 }
 
 export default SideStrana;
