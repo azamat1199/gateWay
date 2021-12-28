1155438f48e4cd2eb0829ecb765e1f8fc0d2ba51
azamat1199 <glaziz1199@gmail.com>
2021-12-28 23:47:06 +0500

    sign up pages

--- src/containers/StudentCabinet/pages/status.jsx
+++ src/containers/StudentCabinet/pages/status.jsx
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import StudentSidebar from "./SidebarStudent";
 import "../../../style/css/status.css";
 import StudentCabinet from "../studentCabinet";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
 import Axios from "../../../utils/axios.js";
+import avatar from "../../../assets/icon/Avatar.svg";
 import wait from "../../../assets/images/Waiting.svg";
 import goal from "../../../assets/images/Goal.svg";
+import styled from "styled-components";
+import Loader from "react-js-loader";
 import { useSelector } from "react-redux";
 const Status = () => {
   const [loading, setLoading] = useState(false);
@@ -13,10 +19,17 @@ const Status = () => {
     university_invoice_confirmed: null,
     university_invoice_upload: null,
   });
+  const [stepNumber, setStepNumber] = useState(-1);
+  const [myData, setMyData] = useState("");
+  const [currentStep, setCurrentStep] = useState("");
   const selector = useSelector((state) => state);
   const { payload } = selector.payload;
   const { first_name, last_name } = payload.data;
-
+  const steps = [
+    "менеджер проверяет",
+    "нотариальная проверка",
+    "универ проверка",
+  ];
   const fetchInvoice = async () => {
     setLoading(true);
     try {
@@ -35,8 +48,45 @@ const Status = () => {
       setLoading(false);
     }
   };
+  const fetchMyData = async () => {
+    setLoading(true);
+    try {
+      const res = await Axios.get("/applicant/me/");
+      const { data, status } = res;
+      console.log(res);
+      if (status == 200) {
+        const { step } = data;
+        setMyData(data);
+        setCurrentStep(step);
+        if (step === "payment_confirmation") {
+          setStepNumber(0);
+        } else if (
+          step === "manager_checking" ||
+          step === "payment_confirmed"
+        ) {
+          setStepNumber(1);
+        } else if (
+          step === "notary" ||
+          step === "manager_reject_notary" ||
+          step === "manager_checking_notary"
+        ) {
+          setStepNumber(2);
+        } else if (step === "university" || step === "rejected") {
+          setStepNumber(3);
+        } else if (step === "completed") {
+          setStepNumber(4);
+        }
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  console.log(myData);
   useEffect(() => {
     fetchInvoice();
+    fetchMyData();
   }, []);
   return (
     <>
@@ -44,32 +94,46 @@ const Status = () => {
       <div className="main">
         <div className="status">
           <div className="top">
-            <h1>Ваши университеты</h1>
+            <h1>Статус ваших документов</h1>
             <div>
-              <img src="https://picsum.photos/70" alt="" />
-              <h2>
-                {first_name} {last_name} <span>Заявитель</span>
-              </h2>
+              <>
+                <img src={avatar} alt="userImage" />
+                <h2>
+                  {first_name} {last_name} <span>Заявитель</span>
+                </h2>
+              </>
             </div>
           </div>
           <div className="bottom">
-            {invoice.university_invoice_confirmed === true ? (
+            <SvgContainer>
+              <Stepper activeStep={stepNumber} alternativeLabel>
+                <Step key="accountant">
+                  <StepLabel>Проверка оплаты</StepLabel>
+                </Step>
+                {steps.map((label) => (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+            </SvgContainer>
+            {invoice.university_invoice_confirmed === true ||
+            currentStep === "completed" ? (
               <>
                 <img src={goal} alt="waitForResult" />
                 <h1>
                   Вы успешно зачислены в университе! <span>Поздравления!</span>
                 </h1>
               </>
-            ) : invoice.university_invoice_confirmed === null ? (
-              "yoi dont have any invoice yet"
-            ) : (
+            ) : currentStep === "rejected" ? (
               <>
                 <img src={wait} alt="waitForResult" />
                 <h1>
-                  В вашем платеже есть недостатки.
-                  <span>Обратитесь менеджеру</span>
+                  Документы расмартиваются .<span>Пожалуйста подождите</span>
                 </h1>
               </>
+            ) : (
+              ""
             )}
           </div>
         </div>
@@ -79,3 +143,10 @@ const Status = () => {
 };
 
 export default Status;
+
+const SvgContainer = styled.div`
+  svg {
+    height: 24px !important;
+    width: 24px !important;
+  }
+`;
